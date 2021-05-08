cbeec16144e1b7d148377ae28d7bee4b60237095
Dipzza <dipzza@disroot.org>
2021-05-08 08:39:56 +0200

    :sparkles::recycle: Implementa funciones base para asignar mandos/teclados #12
    + Mueve tilesets a localización correcta
    + Simplifica código en player y bomb

--- GuerraDeAbrazos/actors/player/bomb.gd
+++ GuerraDeAbrazos/actors/player/bomb.gd
@@ -3,17 +3,21 @@ extends Area2D
 var push_force = 0
 var velocity = Vector2()
 
+
 func start(pos, dir, speed, force):
 	position = pos
 	rotation = dir
 	push_force = force
 	velocity = Vector2(speed, 0).rotated(rotation)
 
+
 func _physics_process(delta):
 	position += velocity * delta
 
-func _on_Bomb_body_shape_entered(body_id, body, body_shape, local_shape):
-	if local_shape == 0:
+
+func _on_body_shape_entered(body_id, body, body_shape, local_shape):
+	# El proyectil ha colisionado, la bomba explota
+	if local_shape == 0 and not body.is_in_group("player"):
 		velocity = Vector2.ZERO
 		$ExplosionCollision.set_deferred("disabled", false)
 		$ProjectileCollision.set_deferred("disabled", true)
@@ -21,10 +25,11 @@ func _on_Bomb_body_shape_entered(body_id, body, body_shape, local_shape):
 		$Timer.start()
 		yield($Timer, "timeout")
 		queue_free()
-		
+	# Ha entrado un cuerpo en la explosión, si es empujable se empuja
 	elif local_shape == 1:
 		if body.has_method("apply_impulse"):
 			body.apply_impulse(global_position.direction_to(body.global_position) * push_force)
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
