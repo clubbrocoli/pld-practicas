a0c502ee4af97dd5f301da6770004ab7af67b79b
Dipzza <dipzza@disroot.org>
2021-05-19 11:03:59 +0200

    :bug::bento: Fix multiple bomb bugs, smooth dash end and change to use full spritesheets in animations
    + Fix: Bomb didn't collide with other bomb due to it not having it's own layer
    + Fix: Bomb spawned inside the lower wall and didn't push the player. This was because it spawned at player center, and player center was lower than desired.
    + Fix: Angle of movemevent was being used as the bomb rotation too, this was fine but can cause problems if you want to start the bomb at any position diferent than (0, 0) or with future animations.
    + Dash Change: The half end duration of dash now has it velocity interpolated to zero by a Tween node, so it has a smooth transition to stopping.
    + Player Sprite Change: Animations now only use one texture, the full spritesheet. This way we can reuse the animations for diferent characters spritesheets just by changing the texture.

--- GuerraDeAbrazos/actors/player/player.gd
+++ GuerraDeAbrazos/actors/player/player.gd
@@ -1,36 +1,40 @@
 extends KinematicBody2D
 
-var player_id = 0
-var device_id = 0
 
-# Movement constants
+const Bomb = preload("res://actors/player/bomb.tscn")
+
 export var max_speed = 200
 export var accel = 1500
 export var friction = 1500
+export var dash_speed = 600
+export var dash_max_charges = 2
+export var bomb_speed = 200
+export var bomb_push = 750
+export var bomb_charges = 2
 
-# Movement variables
-var velocity = Vector2.ZERO
-var impulse_vel = Vector2.ZERO
+var id = 0
+var device_id = 0
 var input = Vector2.ZERO
 var last_input = Vector2.ZERO
-
-# Dash variables
-export var dash_speed = 700
-export var dash_max_charges = 2
+var velocity = Vector2.ZERO
+var impulse_vel = Vector2.ZERO
 var dash_charges = dash_max_charges
 var dashing = false
 
-# Bomb constants
-export var bomb_speed = 200
-export var bomb_push = 750
-var Bullet = preload("res://actors/player/bomb.tscn")
-var bomb_charges = 2
 
-# Controller asignment
-func start(p_id, d_id, pos):
-	player_id = p_id
-	device_id = d_id
-	position = pos
+# Input
+func _input(event):
+	if event.is_action_pressed("bomb_" + str(device_id)):
+		throw_bomb()
+	elif event.is_action_pressed("dash_" + str(device_id)) and not dashing:
+		dash()
+
+
+func get_input():
+	var n = str(device_id)
+	input.x = Input.get_action_strength("mv_right_" + n) - Input.get_action_strength("mv_left_" + n)
+	input.y = Input.get_action_strength("mv_down_" + n) - Input.get_action_strength("mv_up_" + n)
+	input = input.normalized()
 
 
 # Movement
@@ -52,20 +56,6 @@ func _physics_process(delta):
 		var motion = velocity * (1 - impulse_vel.length()/bomb_push)
 		move_and_slide(motion + impulse_vel)
 		impulse_vel = impulse_vel.move_toward(Vector2.ZERO, friction * delta)
-
-
-func _input(event):
-	if event.is_action_pressed("bomb_" + str(device_id)):
-		throw_bomb()
-	elif event.is_action_pressed("dash_" + str(device_id)) and not dashing:
-		dash()
-
-
-func get_input():
-	var n = str(device_id)
-	input.x = Input.get_action_strength("mv_right_" + n) - Input.get_action_strength("mv_left_" + n)
-	input.y = Input.get_action_strength("mv_down_" + n) - Input.get_action_strength("mv_up_" + n)
-	input = input.normalized()
 
 
 func apply_friction(delta):
@@ -96,7 +86,7 @@ func change_animation(animation):
 func throw_bomb():
 	if bomb_charges != 0:
 		bomb_charges -= 1
-		var b = Bullet.instance()
+		var b = Bomb.instance()
 		b.start(global_position, 
 				last_input.angle(), bomb_speed + (velocity + impulse_vel).length() * 0.5, bomb_push, get_instance_id())
 		get_parent().add_child(b)
@@ -119,12 +109,17 @@ func dash():
 
 
 func _on_DashDuration_timeout():
-	velocity = velocity.linear_interpolate(Vector2.ZERO, 1)
-	set_collision_mask_bit(2, true)
-	dashing = false
+	$Tween.interpolate_property(self, "velocity", velocity, Vector2.ZERO, $DashDuration.wait_time, Tween.TRANS_SINE, Tween.EASE_OUT)
+	$Tween.start()
 
 
 func _on_DashCD_timeout():
 	dash_charges += 1
 	if dash_charges == dash_max_charges:
 		$DashCD.stop()
+
+
+func _on_Tween_tween_completed(object, key):
+	if object == self and key == ":velocity":
+		set_collision_mask_bit(2, true)
+		dashing = false
